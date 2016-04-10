08ca8a75094a2009838a632bd686ebc88f1c46ea
Ansho-enigu <ansho-enigu@gmx.co.uk>
2016-04-10 18:54:56 +0100

    Create lamport.cpp

---
+++ src/crypto/lamport.cpp
@@ -0,0 +1,9 @@
+#include "crypto/lamport.h"
+
+#include "crypto/ripemd160.h"
+#include "crypto/common.h"
+
+bool LAMPORT::checksig(unsigned char* data, uint8_t[20] sig, uint8_t[20][160] pubkey) 
+{
+
+}

1257262f55aff0cc3caf9fe4cd4a97e43892b965
Ansho-enigu <ansho-enigu@gmx.co.uk>
2016-04-11 12:22:33 +0100

    Update lamport.h

--- src/crypto/lamport.h
+++ src/crypto/lamport.h
@@ -9,11 +9,11 @@
 class LAMPORT
 {
 private:
-    uint8_t pubkey[20][160];
+    char pubkeys[20][160];
     char[320][20] prikeys;
     
 public:
-    bool checksig(unsigned char* data, uint8_t[20] sig, uint8_t[20][160] pubkey);    /* data is the transaction and sig is 160-bit's  */
+    bool checksig(unsigned char* data, char[160][20] sig, char[20][160] pubkey);    /* data is the transaction and sig is 160-bit's  */
     char[160][20] createsig(unsigned char* data, unsigned uint512_t prikey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
     
 }

0a526df20ddbe6f61d7538cfcbc41fdf6eda270d
Ansho Enigu <ansho-enigu@gmx.co.uk>
2016-04-23 14:45:27 +0100

    Update lamport.h

--- src/crypto/lamport.h
+++ src/crypto/lamport.h
@@ -2,8 +2,7 @@
 #ifndef BITCOIN_CRYPTO_LAMPORT_H
 #define BITCOIN_CRYPTO_LAMPORT_H
 
-#include <stdint.h>
-#include <stdlib.h>
+#include "crypto/lamport.h"
 #include "crypto/ripemd160.h"
 
 class LAMPORT

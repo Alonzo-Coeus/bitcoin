8aba2c03dbd84ec3a8564a3471fbb445c79fe9d2
Ansho Enigu <ansho-enigu@gmx.co.uk>
2016-04-25 16:57:49 +0100

    Update lamport.cpp

--- src/crypto/lamport.cpp
+++ src/crypto/lamport.cpp
@@ -37,7 +37,7 @@
           
     }
 
-    char[320][20] LAMPORT::createsig(unsigned char* data, uint512_t prikey) 
+    char** LAMPORT::createsig(unsigned char* data, uint512_t prikey) 
     {
       /* hash of the message */
       bool messhashb[160];

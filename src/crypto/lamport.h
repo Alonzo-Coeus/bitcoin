31c3d1f3815c48a7d38b945afb94878ab3b0fd36
Ansho Enigu <ansho-enigu@gmx.co.uk>
2016-05-21 20:04:32 +0100

    Update lamport.h

--- src/crypto/lamport.h
+++ src/crypto/lamport.h
@@ -15,9 +15,9 @@ private:
     char prikeys[320][20];
 
 public:
-    static int chuncksize = 1; /* in bytes */
-    bool LAMPORT::checksig(unsigned char data[], char sig[20][2][20], char rootkey[20], char merklewit[]);
-    char * LAMPORT::createsig(unsigned char data[], uint512_t prikey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
+    static const int chuncksize = 1; /* in bytes */
+    bool checksig(unsigned char data[], char sig[20][2][20], char rootkey[20], char merklewit[]);
+    char * createsig(unsigned char data[], uint512_t prikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
     char * createpubkey(uint512_t prikey);
 };
 

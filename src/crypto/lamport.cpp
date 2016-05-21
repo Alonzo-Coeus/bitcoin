287b1c33d0229dca331546897eaba830a711ae04
Ansho Enigu <ansho-enigu@gmx.co.uk>
2016-05-21 20:45:55 +0100

    Update lamport.cpp

--- src/crypto/lamport.cpp
+++ src/crypto/lamport.cpp
@@ -4,7 +4,7 @@
 #include "crypto/ripemd160.h"
 #include "crypto/common.h"
 #include "uint256.h"
-bool LAMPORT::checksig(unsigned char data[], char sig[20][2][20], char rootkey[20], char merklewit[])
+bool LAMPORT::checksig(unsigned char data[10000], char sig[20][2][20], char rootkey[20], char merklewit[])
 {
 
   char exmerklewit[8][20]; /*this is the merkle wit minus the main public key max number of publickeys to rootkey is 256 due to 2^n where n is the first array index is 8*/
@@ -65,17 +65,17 @@ bool LAMPORT::checksig(unsigned char data[], char sig[20][2][20], char rootkey[2
       */
       return true; // if compleats all tests return true
 }
-    char * LAMPORT::createsig(unsigned char data[], uint512_t prikey, int sellectedpubkey)
+    char * LAMPORT::createsig(unsigned char data[10000], uint512_t prikey, int sellectedpubkey)
     {
       /* hash of the message */
       bool messhashb[160];
-      unsigned char[] messhash;
+      unsigned char messhash[20];
       CRIPEMD160().Write(&data, sizeof(data)).Finalize(&messhash);
 
       /* creating true key from seed (the seed is used as the key by the user but it only is a form of compress key) */
       unsigned char* vchHash
       CRIPEMD160().Write(&prikey, sizeof(prikey)).Finalize(&vchHash);
-      char tempHash[];
+      char tempHash[20];
       for(int i =0; i < 320; i++)
       {
         CRIPEMD160().Write(&vchHash, sizeof(vchHash)).Write(&i, sizeof(i)).Finalize(&tempHash);
@@ -84,17 +84,7 @@ bool LAMPORT::checksig(unsigned char data[], char sig[20][2][20], char rootkey[2
 
       /* the signing will happen uder this */
       char sig[320][20];
-      memcpy(&messhashb, &messhash, sizeof(messhashb));
-      for(int i=0; i < 160; i++)
-      {
-        if(messhashb[i])
-        {
-          sig[i] = prikeys[2*i];
-        }
-        else
-        {
-          sig[i] = prikeys[(2*i)+1];
-        }
-      }
+
+
       return &sig;
     }

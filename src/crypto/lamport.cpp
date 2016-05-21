e2852098e9991bb8a3708a7fd6e1799f0161c44c
Ansho Enigu <ansho-enigu@gmx.co.uk>
2016-05-21 17:38:10 +0100

    Add files via upload

--- src/crypto/lamport.cpp
+++ src/crypto/lamport.cpp
@@ -3,60 +3,64 @@
 
 #include "crypto/ripemd160.h"
 #include "crypto/common.h"
-    bool LAMPORT::checksig(char data[], char sig[160][20], char pubkey[320][20]) 
+namespace
+{
+  namespace lamport
+  {
+    bool LAMPORT::checksig(unsigned char* data, char[160][20] sig, char[20][160] pubkey) 
     {
-        //use a for loop which terminates if not valid then function returns var valid
             bool messhashb[160];
-            unsigned char* messhash;
-            CRIPEMD160().Write(&data, sizeof(data)).Finalize(&messhash);
+            valtype messhash(true ? 20 : 32);
+            CRIPEMD160().Write(begin_ptr(data), data.size()).Finalize(begin_ptr(messhash));
             memcpy(messhashb, messhash, sizeof(messhashb));
             
             char _sig[160][20];
             char _csig[160][20];
+            
             for(int i=0; i < 160; i++)
             {
               for(int o=0; o < 20; o++)
               {
                   if(messhashb[i]) 
                   {
-                    _sig[i][o] = pubkey[2*i][o];
+                    _sig[o][i] = pubkey[o][2*i];
                   }
                   else
                   {
-                    _sig[i][o] = pubkey[(2*i)+1][o];
+                    _sig[o][i] = pubkey=[o][(2*i)+1];
                   }
               }
             }
             
-            unsigned char* sighop;
+            valtype sighop(true ? 20 : 32)
             for(int i=0; i < 160; i++)
             {
-              CRIPEMD160().Write(&_sig[i], sizeof(_sig[i])).Finalize(&sighop);
-              memcpy(_csig[i], sighop, sizeof(_csig[i]));
+              CRIPEMD160().Write(begin_ptr(_sig[i]), _sig[i].size()).Finalize(begin_ptr(sighop));
+              memcpy(_csig[i], sighop, _csig[i].size());
             }
             return sig == _csig;
           
     }
 
-    char [160][20]LAMPORT::createsig(char data[], uint512_t prikey) 
+    char[160][20] LAMPORT::createsig(unsigned char* data, unsigned uint512_t prikey) 
     {
       /* hash of the message */
       bool messhashb[160];
-      unsigned char* messhash;
-      CRIPEMD160().Write(&data, sizeof(data)).Finalize(&messhash);
+      valtype messhash(true ? 20 : 32);
+      CRIPEMD160().Write(begin_ptr(data), data.size()).Finalize(begin_ptr(messhash));
       
       /* creating true key from seed (the seed is used as the key by the user but it only is a form of compress key) */
-      unsigned char* vchHash
-      CRIPEMD160().Write(&prikey, sizeof(prikey)).Finalize(&vchHash);
-      char tempHash[];
+      valtype vchHash(true ? 20 : 32);
+      CRIPEMD160().Write(begin_ptr(prikey), prikey.size()).Finalize(begin_ptr(vchHash));
       for(int i =0; i < 320; i++) 
       {
-        CRIPEMD160().Write(&vchHash, sizeof(vchHash)).Write(&i, sizeof(i)).Finalize(&tempHash);
+        valtype tempHash(true ? 20 : 32);
+        CRIPEMD160().Write(begin_ptr(vchHash), prikey.size()).Write(i, i.size()).Finalize(begin_ptr(tempHash));
         prikeys[i] = temphash;
       }
       
       /* the signing will happen uder this */
-      char sig[320][20];
+      char[160][20] sig;
       memcpy(messhashb, messhash, sizeof(messhashb));
       for(int i=0; i < 160; i++)
       {
@@ -71,3 +75,5 @@
       }
       return sig;
     }
+  }
+}

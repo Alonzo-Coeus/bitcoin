263b4fd9f113c012839a35e51691128322e0fe0d
Ansho Enigu <ansho-enigu@gmx.co.uk>
2016-05-21 17:57:05 +0100

    Add files via upload

--- lamport.h
+++ lamport.h
@@ -2,22 +2,20 @@
 #ifndef BITCOIN_CRYPTO_LAMPORT_H
 #define BITCOIN_CRYPTO_LAMPORT_H
 
-#include <boost/multiprecision/cpp_int.hpp>
-
+#include <stdint.h>
+#include <stdlib.h>
 #include "crypto/ripemd160.h"
-
-using namespace boost::multiprecision;
 
 class LAMPORT
 {
 private:
-    char pubkeys[320][20];
-    char prikeys[320][20];
-
+    char pubkeys[20][160];
+    char[320][20] prikeys;
+    
 public:
-    bool checksig(char data[], char sig[160][20], char pubkey[320][20]);    /* data is the transaction and sig is 160-bit's  */
-    char[160][20] createsig(char data[], uint512_t prikey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
-    char[320][20] createpubkey(uint512_t prikey);
-};
+    bool checksig(unsigned char* data, char[160][20] sig, char[20][160] pubkey);    /* data is the transaction and sig is 160-bit's  */
+    char[160][20] createsig(unsigned char* data, unsigned uint512_t prikey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
+    
+}
 
-#endif // BITCOIN_CRYPTO_LAMPORT_H
+#endif

4c2b374fb29a8920f0515e6c0e0f3a08ca1fca2c
Alonzo Coueus <alonzo.coeus@mail.com>
2016-05-22 17:38:04 +0100

    Update lamport.h

--- src/crypto/lamport.h
+++ src/crypto/lamport.h
@@ -13,14 +13,14 @@ class LAMPORT
 private:
     char pubkeys[320][20];
     char prikeys[320][20];
-
+    char sig[20][2][20];
 public:
     static const int chuncksize = 1; /* in bytes */
     bool checksig(unsigned char data[10000], char sig[20][2][20], char rootkey[20], char merklewit[]);
-    char *** createsig(unsigned char data[10000], uint512_t prikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
-    char * createmerklewit(uint512_t prikey, int sellectedpubkey);
-    char * createrootkey(uint512_t prikey);
-    char * createpubkey(uint512_t prikey);
+    int createsig(unsigned char data[10000], uint512_t prikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
+    int createmerklewit(uint512_t prikey, int sellectedpubkey);
+    int createrootkey(uint512_t prikey);
+    int createpubkey(uint512_t prikey);
 };
 
 #endif // BITCOIN_CRYPTO_LAMPORT_H

b7b32e08b371fa1a1d8ea6e2eddc1e653ee7ff85
Alonzo Coueus <alonzo.coeus@mail.com>
2016-05-23 13:13:47 +0100

    Update lamport.cpp

--- src/crypto/lamport.cpp
+++ src/crypto/lamport.cpp
@@ -6,6 +6,7 @@
 #include "uint256.h"
 
 using namespace std;
+typedef vector<unsigned char> valtype;
 
 bool LAMPORT::checksig(unsigned char data[10000], char sig[20][2][20], char rootkey[20], char merklewit[])
 {
@@ -46,8 +47,9 @@ bool LAMPORT::checksig(unsigned char data[10000], char sig[20][2][20], char root
   //end decoding merkle wit format
 
   //start checking if new publickey is a part of the root key
-  char tempverifyhash[20];
-  CRIPEMD160().Write(begin_ptr(pubkey), 800).Finalize(begin_ptr(tempverifyhash)); //first element is start of arrays address length pre-def
+  valtype& vch = pubkey;
+  valtype tempverifyhash;
+  CRIPEMD160().Write(begin_ptr(vch), 800).Finalize(begin_ptr(tempverifyhash)); //first element is start of arrays address length pre-def
   for(int i = 0; true; i++) //to end if false we will use return to lower processing time
   {
     if(exmerklewit[i][0] == 0 && exmerklewit[i][1] == 0 && exmerklewit[i][2] == 0 && exmerklewit[i][3] == 0)
@@ -61,8 +63,8 @@ bool LAMPORT::checksig(unsigned char data[10000], char sig[20][2][20], char root
         return false;
       }
     }
-
-    CRIPEMD160().Write(begin_ptr(tempverifyhash), tempverifyhash.size()).Write(begin_ptr(exmerklewit), exmerklewit.size()).Finalize(begin_ptr(tempverifyhash));
+    vch = exmerklewit;
+    CRIPEMD160().Write(begin_ptr(vch), vch.size()).Write(begin_ptr(tempverifyhash), tempverifyhash.size()).Finalize(begin_ptr(tempverifyhash));
   }
 
   //end checking if new publickey is a part of the root key

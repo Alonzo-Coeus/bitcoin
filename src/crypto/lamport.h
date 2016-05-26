969789d4b5433d0074d542ebeba26e7e16af4b96
Alonzo Coueus <alonzo.coeus@mail.com>
2016-05-26 16:36:29 +0100

    Update lamport.h

--- src/crypto/lamport.h
+++ src/crypto/lamport.h
@@ -7,6 +7,7 @@
 #include "crypto/ripemd160.h"
 
 using namespace boost::multiprecision;
+typedef std::vector<unsigned char> valtype;
 
 class LAMPORT
 {
@@ -16,7 +17,7 @@ private:
     char sig[20][2][20];
 public:
     static const int chuncksize = 1; /* in bytes */
-    bool checksig(unsigned char data[10000], char sig[20][2][20], char rootkey[20], char merklewit[]);
+    bool checksig(valtype data, char sig[20][2][20], valtype rootkey, valtype merklewit);
     char *createsig(unsigned char data[10000], uint512_t prikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
     char *createmerklewit(uint512_t prikey, int sellectedpubkey);
     char *createrootkey(uint512_t prikey);

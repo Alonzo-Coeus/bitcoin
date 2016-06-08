3a9b89e69727687a1852ebcb9a5c9a7f36dbad1b
Alonzo Coueus <alonzo.coeus@mail.com>
2016-06-08 13:12:39 +0100

    Update lamport.h

--- src/crypto/lamport.h
+++ src/crypto/lamport.h
@@ -17,8 +17,8 @@ private:
     char sig[20][2][20];
 public:
     static const int chuncksize = 1; /* in bytes */
-    bool checksig(valtype data, char sig[20][2][20], valtype rootkey, valtype merklewit);
-    char *createsig(unsigned char data[10000], uint512_t prikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
+    bool checksig(valtype *data, valtype *asig, valtype *rootkey, valtype *merklewit);
+    char *createsig(valtype *data, uint512_t *prikey, int sellectedpubkey);                 /* data is data to be signed and prikey is a sudo-random num gen seed */
     char *createmerklewit(uint512_t prikey, int sellectedpubkey);
     char *createrootkey(uint512_t prikey);
     char *createpubkey(uint512_t prikey);
